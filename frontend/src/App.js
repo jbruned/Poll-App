7544ab874221e0262c152a759142bf6de94d238c
Jorge Bruned <jbruned@veridas.com>
2023-03-15 19:16:19 +0100

    Create empty react application Related to #5

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Empty <code>React</code> application
+        </p>
+        <small>
+          Hello world!
+        </small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
