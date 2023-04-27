75c12f032107d212b07b81d98299006a3e8cfa56
Jorge Bruned <bruned.127631@e.unavarra.es>
2023-04-27 02:32:49 +0200

    Reformat frontend code, fix linter issues

--- frontend/src/components/layoutComponents.js
+++ frontend/src/components/layoutComponents.js
@@ -1,10 +1,7 @@
 import {
     Link,
-    Outlet,
-    useNavigate
+    Outlet
 } from "react-router-dom";
-import Swal from "sweetalert2";
-import { apiRequest, myAlert, swalInput } from "../util";
 import { Icon } from "./basicComponents";
 
 export function BasicLayout(props) {
@@ -23,28 +20,6 @@ export function BasicLayout(props) {
 }
 
 export function MainLayout(props) {
-
-    const navigate = useNavigate()
-
-    function createPoll() {
-        swalInput('Create poll', 'Enter poll title', 'text', 'Create', title => {
-            apiRequest('polls', 'POST', {
-                title: title
-            }).then(res => {
-                if (res.id)
-                    navigate(`/poll/${res.id}`)
-                else
-                    myAlert('Something went wrong', 'An error occurred while creating the poll', 'error')
-            })
-        })
-    }
-
-    function adminLogin() {
-        swalInput('Admin login', 'Enter admin password', 'password', 'Login', props.doLogin)
-            .then(() => { Swal.close() })
-            .catch(() => { Swal.setValidationMessage('Incorrect password') })
-    }
-
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -55,31 +30,7 @@ export function MainLayout(props) {
                     </button>
                     <div id="navbar-content" className="collapse navbar-collapse">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            {/*<li className="nav-item">
-                                <Link to="/" className="nav-link text-white">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                    <Link to="/edit" className="nav-link text-white">Create poll</Link>
-                                </li>
-                                {(props.admin ?? false) ? (
-                                    <li className="nav-item dropdown">
-                                        <a href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" className="nav-link dropdown-toggle text-light">Admin</a>
-                                        <ul aria-labelledby="admin-dropdown" className="dropdown-menu dropdown-menu-end">
-                                            <li>
-                                                <Link to="/admin/tokens" className="dropdown-item">Change password</Link>
-                                            </li>
-                                            <li>
-                                                <Link to="/admin/stats" className="dropdown-item">Logout</Link>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                ) : (
-                                    <li className="nav-item">
-                                        <a href="#" className="nav-link text-white" onClick={adminLogin}>Login</a>
-                                    </li>
-                                )*/}
                             <li>
-                                {/* <button role="button" onClick={props.toggleDarkMode} className="dropdown-item">Toggle dark mode</button> */}
                                 <div className="my-switch">
                                     <input id="my-switch" className="check-toggle check-toggle-round-flat" type="checkbox" checked={props.darkMode} onChange={props.toggleDarkMode} />
                                     <label htmlFor="my-switch" />
