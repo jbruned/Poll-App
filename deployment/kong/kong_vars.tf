939e4201c23ebd1e297e0b9d5136fca04cb27c66
Jorge Bruned <bruned.127631@e.unavarra.es>
2023-05-28 14:27:18 +0200

    Minor GitHub env URL output fix

--- deployment/kong/kong_vars.tf
+++ deployment/kong/kong_vars.tf
@@ -12,19 +12,3 @@ variable "API_BASE_URL" {
 	type    = string
 	default = "/api/v1"
 }
-
-variable "SSL_CERT_BASE64" {
-	type      = string
-	sensitive = true
-	default   = ""
-}
-
-variable "SSL_KEY_BASE64" {
-	type      = string
-	sensitive = true
-	default   = ""
-}
-
-locals {
-	USE_SSL = var.SSL_CERT_BASE64 != "" && var.SSL_KEY_BASE64 != ""
-}
